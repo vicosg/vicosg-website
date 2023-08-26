1ffaf80192081dc71d4817a75db334722cebe384
Javier <linkjavier@hotmail.com>
2023-08-25 20:15:41 -0500

    Changes on default colors

--- components/sections/homeSection.jsx
+++ components/sections/homeSection.jsx
@@ -18,7 +18,7 @@ const HomeSection = props => {
           <div className="xl:translate-y-0 translate-y-32 mr-2 text-xl text-right xl:mr-10 font-lato-black">
               {topText}
           </div>
-          <div className="xl:translate-y-0 translate-y-32 xl:mr-10 mr-2 pr-2 pl-2 h-12 xl:text-6xl text-5xl bg-[#e56608ff] ml-auto text-right self-center font-extrabold hover:scale-110 transition duration-500">
+          <div className="xl:translate-y-0 translate-y-32 xl:mr-10 mr-2 pr-2 pl-2 h-12 xl:text-6xl text-5xl bg-[#ffb300] ml-auto text-right self-center font-extrabold hover:scale-110 transition duration-500">
             <div className="-translate-y-1 font-lato-black text-right">
               {bottomText}
             </div>
